ffb4222a752ffe9a5457d83f0dd378c8a64a62ae
泽梦 <administer@mengze.vip>
2025-03-10 14:20:33 +0800

    update proxyCore.js

--- src/proxyCore.js
+++ src/proxyCore.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = await import('node-fetch').then(mod => mod.default);
 
 module.exports.handler = async (event) => {
     const path = event.path.replace(/^\/proxy\//, '');
